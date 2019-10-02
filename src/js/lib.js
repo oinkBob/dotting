4d9f5ddaadaa94e804ab4e2b20b9238e2e48b304
oinkbob <diwidan@gmail.com>
2019-10-02 09:04:25 +0200

    dotting proto

---
+++ src/js/lib.js
@@ -0,0 +1,53 @@
+/*jshint esversion: 6 */
+/*jshint -W030*/
+
+let keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false
+};
+
+window.addEventListener('keydown', function (e) {
+    keys[e.code] = true;
+});
+
+window.addEventListener('keyup', function (e) {
+    keys[e.code] = false;
+});
+let element = document.body;
+
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+    keycode.info
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+//Enter (for FullScreen)
+document.addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) {
+        requestFullScreen(element);
+    }
+}, false);
+
+//Gamepad
+window.addEventListener("gamepadconnected", function(e) {
+    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+      e.gamepad.index, e.gamepad.id,
+      e.gamepad.buttons.length, e.gamepad.axes.length);
+  });
+
+//Implement hit oder shoot or smth ALSO Controller 
+//https://keycode.info
+// document.addEventListener("keypress", function (e) {
+//     if (e.keyCode === 87) {
+//         requestFullScreen(element);
+//     }
+// }, false);
---
+++ src/js/work.js
@@ -0,0 +1,104 @@
+/*jshint esversion: 6 */
+/*jshint -W030*/
+
+class Player {
+   constructor(x, y, r) {
+      this.x = x,
+         this.y = y,
+         this.r = r,
+         this.velocity = {
+            x: 1,
+            y: 1
+         };
+   }
+
+   draw(c) {
+      c.save();
+      c.beginPath();
+      c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+      c.fillStyle = "#fff";
+      c.shadowColor = "#e3eaef";
+      c.shadowBlur = 20;
+      c.fill();
+      c.closePath();
+      c.restore();
+   }
+
+   update(c) {
+      if (this.x + this.r > c.canvas.width) {
+         this.x = 0 + this.r;
+      }
+      if (this.x < 0 + this.r) {
+         this.x = c.canvas.width - this.r;
+      }
+      if (this.y > c.canvas.height - this.r) {
+         this.y = 0 + this.r;
+      }
+      if (this.y - this.r < 0) {
+         this.y = c.canvas.height - this.r;
+      }
+
+      if (keys.ArrowUp) {
+         this.y -= this.velocity.y;
+      }
+      if (keys.ArrowDown) {
+         this.y += this.velocity.y;
+      }
+      if (keys.ArrowLeft) {
+         this.x -= this.velocity.x;
+      }
+      if (keys.ArrowRight) {
+         this.x += this.velocity.x;
+      }
+      this.draw(c);
+
+   }
+}
+class Homebase {
+   constructor(x, y) {
+      this.x = x;
+      this.y = y;
+   }
+   update(c) {
+      this.draw(c);
+   }
+   draw(c) {
+      c.save();
+      c.beginPath();
+      c.arc(this.x + 100, this.y + 100, 50, 0, Math.PI * 2, false);
+      c.fillStyle = "black";
+      c.shadowColor = "#e3eaef";
+      c.shadowBlur = 20;
+      c.fill();
+      c.closePath();
+      c.restore();
+   }
+}
+
+/* Canvas*/
+class PlayingField {
+   constructor() {
+      this.canvas = document.querySelector('canvas');
+      this.ctx = this.canvas.getContext('2d');
+      this.cw = this.canvas.width = window.innerWidth;
+      this.ch = this.canvas.height = window.innerHeight;
+      this.r = Math.random() * 100;
+      this.ufo = new Player(this.cw / 2, this.ch / 2, this.r);
+      this.homebase = new Homebase(this.cw / 2 + 50, this.ch / 2 + 50);
+      this.ctx.fillStyle = "grey";
+   }
+
+   animate() {
+      window.requestAnimationFrame(this.animate.bind(this));
+      this.cw = this.canvas.width = window.innerWidth;
+      this.ch = this.canvas.height = window.innerHeight;
+      this.ctx.fillStyle = "grey";
+      this.ctx.fillRect(0, 0, this.cw, this.ch);
+      this.homebase.update(this.ctx);
+      this.ufo.update(this.ctx);
+
+   }
+}
+
+let canv = new PlayingField();
+canv.animate();
