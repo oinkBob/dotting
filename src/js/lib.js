2d74599cc841948c71ae9fb5e816ae3cf13f37c2
oinkbob <diwidan@gmail.com>
2019-10-02 09:43:50 +0200

    typo

--- src/js/lib.js
+++ src/js/lib.js
@@ -20,7 +20,7 @@ let element = document.body;
 function requestFullScreen(element) {
     // Supports most browsers and their versions.
     var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
-    keycode.info
+    keycode.info;
     if (requestMethod) { // Native full screen.
         requestMethod.call(element);
     } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
