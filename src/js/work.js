4db931db6ba1903a4d7cf59107e925e003045fd0
oinkbob <diwidan@gmail.com>
2019-10-05 15:19:12 +0200

    drive into homebase to let it grow, also boundries

--- src/js/work.js
+++ src/js/work.js
@@ -6,10 +6,14 @@ class HUD {
       this.y = y;
    }
    writeToDisplay(c) {
+      c.save();
       c.font = "30px Comic Sans MS";
       c.fillStyle = "red";
-      c.textAlign = "center";
+      c.textAlign = "center";      
+      c.shadowColor = "#e3eaef";
+      c.shadowBlur = 10;
       c.fillText("Statistics", this.x, this.y);
+      c.restore();
    }
 }
 class Player {
@@ -25,25 +29,29 @@ class Player {
    }
 
    draw(c) {
+      c.save();
       c.beginPath();
       c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
       c.fillStyle = "#fff";
+      c.shadowColor = "#e3eaef";
+      c.shadowBlur = 10;
       c.fill();
       c.closePath();
+      c.restore();
    }
 
    update(c) {
-      if (this.x + this.r > c.canvas.width) {
-         this.x = 0 + this.r;
+      if (this.x + this.r > 0.95 * c.canvas.width) {
+         this.x = 0 + this.r + c.canvas.width * 0.05;
       }
-      if (this.x < 0 + this.r) {
-         this.x = c.canvas.width - this.r;
+      if (this.x  < c.canvas.width * 0.05 + this.r) {
+         this.x = c.canvas.width * 0.95 - this.r;
       }
-      if (this.y > c.canvas.height - this.r) {
-         this.y = 0 + this.r;
+      if (this.y > c.canvas.height * 0.95 - this.r) {
+         this.y = 0 + c.canvas.width * 0.05 + this.r;
       }
-      if (this.y - this.r < 0) {
-         this.y = c.canvas.height - this.r;
+      if (this.y - this.r - c.canvas.width * 0.05 < 0) {
+         this.y = c.canvas.height * 0.95 - this.r;
       }
 
       if (keys.ArrowUp) {
@@ -62,6 +70,26 @@ class Player {
 
    }
 }
+class Enemy {
+   constructor(x, y, r, z){
+      this.x = x;
+      this.y = y;
+      this.r = r;
+      this.z = 0;
+   }
+   draw(c) {
+      c.save();
+      c.beginPath();
+      c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+      c.fillStyle = "red";
+      c.shadowColor = "#e3eaef";
+      c.shadowBlur = 10;
+      c.fill();
+      c.closePath();
+      c.restore();
+   }
+
+}
 class Homebase {
    constructor(x, y, r, grow) {
       this.x = x;
@@ -72,15 +100,17 @@ class Homebase {
    update(c) {
       this.collPlayerAndHomebase();
       this.draw(c);
-      
    }
    draw(c) {
-    
+      c.save();
       c.beginPath();
       c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
       c.fillStyle = "black";
+      c.shadowColor = "#e3eaef";
+      c.shadowBlur = 10;
       c.fill();
       c.closePath();
+      c.restore();
 
    }
    collPlayerAndHomebase() {
@@ -89,25 +119,30 @@ class Homebase {
       let distance = (dx * dx + dy * dy);
       let radii = (canv.playerMain.r+canv.homebase.r)*(canv.playerMain.r+canv.homebase.r);
       if (distance <= radii){
-         this.r += 1;
+         this.r += 0.1;
       }
    }
 }
 
 /* Canvas*/
-class PlayingField {
+class initPlayingField {
    constructor() {
       this.canvas = document.querySelector('canvas');
       this.ctx = this.canvas.getContext('2d');
       this.cw = this.canvas.width = window.innerWidth;
       this.ch = this.canvas.height = window.innerHeight;
-      this.r = 30;
+      if (this.cw>this.ch){
+         this.r = this.cw/80;
+      }
+      else{
+         this.r = this.ch/80;
+      }
+      let hudSpace = this.cw/ 2 - this.ch;
       this.xPlayer = Math.floor(Math.random() * (this.cw + 2*this.r - 2*this.r + 1) + 2*this.r );
       this.yPlayer = Math.floor(Math.random() * (this.ch + 2*this.r - 2*this.r + 1) + 2*this.r );
-      this.color = "black";
       this.playerMain = new Player(this.xPlayer, this.yPlayer, this.r);
       this.homebase = new Homebase(this.cw / 2, this.ch / 2, this.r, false);
-      this.hud = new HUD(this.cw / 2, 50);
+      this.hud = new HUD(this.cw / 2, hudSpace);
       this.ctx.fillStyle = "grey";
    }
 
@@ -120,12 +155,8 @@ class PlayingField {
       this.homebase.update(this.ctx);
       this.playerMain.update(this.ctx);
       this.hud.writeToDisplay(this.ctx);
-      
    }
 }
 
-// util
-
-
-let canv = new PlayingField();
+let canv = new initPlayingField();
 canv.animate();
